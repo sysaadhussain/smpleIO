789db95493810fb9aee5cecb2a92a8a60829edc4
saad <sy.2.hussain@gmail.com>
2013-08-28 16:11:54 +0530

    first commit

---
+++ smpleIO.cpp
@@ -0,0 +1,16 @@
+// smpleIO.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	char text[10];
+	cin>>text;
+	cout<<text;
+	return 0;
+}
+
